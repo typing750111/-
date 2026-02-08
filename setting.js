512abd0edca7a91a8f23a8b84b77c1cb0a95dbbf
typing750111 <zakariya750111@gmail.com>
2026-02-08 07:53:10 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 乙卂Ҝ卂尺丨ㄚ卂 𝓑𝓞𝓣 𓆪',
+  author: '‎',
+  botName: "𓆩 乙卂Ҝ卂尺丨ㄚ卂 𝓑𝓞𝓣 𓆪",
+  botOwner: 'Zakariya', // Your name
+  ownerNumber: '923102392745', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/typing750111/-",
+};
+
+module.exports = settings;
